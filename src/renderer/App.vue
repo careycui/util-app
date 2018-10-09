2afa145ccad24a9819388eb20b3e8fa2bb22472c
careycui <haiyucui@gmail.com>
2018-10-09 21:54:36 +0800

    add elementui

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -11,5 +11,16 @@
 </script>
 
 <style>
-  /* CSS */
+*{
+	margin: 0;
+	padding: 0;
+	font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+}
+
+html,body{
+	height: 100%;
+}
+#app{
+	height: 100%;
+}
 </style>
---
+++ src/renderer/components/home.vue
@@ -0,0 +1,45 @@
+<template>
+	<el-container class="home">
+  		<el-header style="height:40px;">Header</el-header>
+		<el-container>
+			<el-aside width="160px">
+				<el-menu>
+					<el-menu-item index="0">
+				        <i class="el-icon-edit"></i>
+				        <span slot="title">代码片段</span>
+			      	</el-menu-item>
+				</el-menu>
+			</el-aside>
+			<el-main>Main</el-main>
+		</el-container>
+	</el-container>
+</template>
+<script>
+	export default{
+		name: 'home'
+	}
+</script>
+<style>
+.el-header{
+    background-color: #20a0ff;
+    background-image: linear-gradient(45deg,#20a0ff,#a8de69);
+    color: #333;
+    text-align: center;
+    line-height: 40px;
+}
+  
+.el-aside {
+	border-right: 1px solid #dcdfe6;
+	color: #333;
+	text-align: center;
+}
+
+.el-main {
+	background-color: #E9EEF3;
+	color: #333;
+	text-align: center;
+}
+.home{
+	height: 100%;
+}
+</style>
